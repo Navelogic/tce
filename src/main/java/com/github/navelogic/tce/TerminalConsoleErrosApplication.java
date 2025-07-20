f673a1184c7277480c52f529a61977e1bbbd4a71
Navelogic <weslleysantoszbd@gmail.com>
2025-07-19 22:23:04 -0300

    spring boot init

---
+++ src/main/java/com/github/navelogic/tce/TerminalConsoleErrosApplication.java
@@ -0,0 +1,13 @@
+package com.github.navelogic.tce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalConsoleErrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminalConsoleErrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/navelogic/tce/TerminalConsoleErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.navelogic.tce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalConsoleErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
